698ab46dc612977ab771255e45efd4206f3fbb50
Allana Larissa <larissaleonel98@gmail.com>
2023-08-16 15:57:59 -0300

    add method listar

--- src/main/java/med/voli/api/controller/MedicoController.java
+++ src/main/java/med/voli/api/controller/MedicoController.java
@@ -3,11 +3,14 @@ package med.voli.api.controller;
 import jakarta.validation.Valid;
 import med.voli.api.endereco.Endereco;
 import med.voli.api.medico.DadosCadastroMedico;
+import med.voli.api.medico.DadosListagemMedicos;
 import med.voli.api.medico.Medico;
 import med.voli.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,6 +22,9 @@ public class MedicoController {
     public void cadastrar (@RequestBody  @Valid DadosCadastroMedico dados){
        repository.save(new Medico(dados));
    }
-
+ @GetMapping
+   public List <DadosListagemMedicos> listar (){
+       return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+ }
 
 }
